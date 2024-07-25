ab3e1259f3fe858215ed0bd36141a9d16e2ed6d3
Alyssa Llanes <41835506+strangeanomalyy@users.noreply.github.com>
2024-07-25 21:59:09 +0800

    Create ff_challenge_3.sql

---
+++ answers/ff_challenge_3.sql
@@ -0,0 +1,43 @@
+-- https://frostyfriday.org/blog/2022/07/15/week-3-basic/
+
+create or replace stage frosty_friday.challenges.frosty_friday_challenge_3_stage
+    url = 's3://frostyfridaychallenges/challenge_3/';
+
+ls '@frosty_friday_challenge_3_stage';
+
+create or replace table challenge_3_keywords as (
+select $1 as keyword, $2 as added_by, $3 as nonsense from '@frosty_friday_challenge_3_stage/keywords.csv' where keyword != 'keyword'
+);
+
+select * from challenge_3_keywords;
+
+create or replace file format csv
+    type = CSV
+    skip_header = 1;
+
+create or replace table challenge_3 as (
+with keywords as (
+    select keyword from challenge_3_keywords
+),
+
+files as (
+    select $1,$2,$3,$4,$5, metadata$filename as filename from '@frosty_friday_challenge_3_stage' (file_format => 'csv')
+),
+
+join_keywords as (
+    select * from files f
+    join keywords k
+    on contains(filename, k.keyword)
+),
+
+count_rows as (
+    select
+        filename,
+        count(*) as number_of_rows
+    from join_keywords
+    group by all
+)
+
+select * from count_rows);
+
+select * from challenge_3;
